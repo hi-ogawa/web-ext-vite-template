802badd893c2dfe573906fbf8d019ac75b651ee3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-06 22:33:13 +0900

    wip: explore iframe approach on dev

--- src/options/app.tsx
+++ src/options/app.tsx
@@ -1,3 +1,25 @@
+import React from "react";
+// import browser from "webextension-polyfill";
+
 export function App() {
-  return <div className="text-blue text-2xl">options</div>;
+  const [counter, setCounter] = React.useState(0);
+  // React.useEffect(() => {
+  //   (async () => {
+  //     const tabs = await browser.tabs.query({});
+  //     console.log(tabs);
+  //   })();
+  // }, []);
+
+  return (
+    <div className="flex flex-col">
+      <div>
+        <h1>Tab Manager</h1>
+        <div className="flex items-center gap-1">
+          <div>{counter}</div>
+          <button onClick={() => setCounter(counter + 1)}>+1</button>
+          <button onClick={() => setCounter(counter - 1)}>-1</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/popup/app.tsx
+++ src/popup/app.tsx
@@ -1,4 +1,14 @@
+// import React from "react";
+// import browser from "webextension-polyfill";
+
 export function App() {
+  // React.useEffect(() => {
+  //   (async () => {
+  //     const tabs = await browser.tabs.query({});
+  //     console.log(tabs);
+  //   })();
+  // }, []);
+
   return (
     <div className="w-[200px] flex flex-col gap-2">
       <button>Save all tabs</button>
