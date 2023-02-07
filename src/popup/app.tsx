dd4dd05cbf6e840cc6eb0667ba713dd0d5a95604
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-08 00:10:13 +0900

    feat: tabs.query (wip)

--- src/popup/app.tsx
+++ src/popup/app.tsx
@@ -3,9 +3,34 @@ import browser from "webextension-polyfill";
 export function App() {
   return (
     <div className="w-[200px] flex flex-col gap-2">
-      <button>Save all tabs</button>
-      <button>Save current tab</button>
-
+      <button
+        onClick={async () => {
+          const tabs = await browser.tabs.query({
+            currentWindow: true,
+            pinned: false,
+            active: true,
+          });
+          const currentTab = tabs[0];
+          console.log({ currentTab });
+          if (currentTab) {
+            // TODO: save
+          }
+        }}
+      >
+        Save current tab
+      </button>
+      <button
+        onClick={async () => {
+          const tabs = await browser.tabs.query({
+            currentWindow: true,
+            pinned: false,
+          });
+          console.log({ tabs });
+          // TODO: save
+        }}
+      >
+        Save all tabs
+      </button>
       <button
         onClick={() => {
           browser.runtime.openOptionsPage();
