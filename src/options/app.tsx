57795a547818f21790b6daf45e8d3bb4a7c27e85
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-07 12:49:31 +0900

    chore: simplify example

--- src/options/app.tsx
+++ src/options/app.tsx
@@ -3,6 +3,8 @@ import browser from "webextension-polyfill";
 
 export function App() {
   const [counter, setCounter] = React.useState(0);
+  const [tabs, setTabs] = React.useState<browser.Tabs.Tab[]>([]);
+
   React.useEffect(() => {
     (async () => {
       const tabs = await browser.tabs.query({});
@@ -11,14 +13,29 @@ export function App() {
   }, []);
 
   return (
-    <div className="flex flex-col">
-      <div>
-        <h1>Tab Manager</h1>
-        <div className="flex items-center gap-1">
-          <div>{counter}</div>
-          <button onClick={() => setCounter(counter + 1)}>+1</button>
-          <button onClick={() => setCounter(counter - 1)}>-1</button>
+    <div className="flex flex-col gap-2">
+      <h1>Options Page</h1>
+      <div className="flex items-center gap-2">
+        <div>Counter = {counter}</div>
+        <div className="flex gap-1">
+          <button onClick={() => setCounter(counter + 1)}>-1</button>
+          <button onClick={() => setCounter(counter - 1)}>+1</button>
         </div>
+      </div>
+      <div>
+        <button
+          onClick={async () => {
+            const tabs = await browser.tabs.query({});
+            setTabs(tabs);
+          }}
+        >
+          List Tabs
+        </button>
+        <ul>
+          {tabs.map((tab) => (
+            <li id={String(tab.id)}>{tab.url}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
