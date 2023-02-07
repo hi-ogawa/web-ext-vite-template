ea825ea4f857967f744201f2b19f7f6bd6e8e22a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-07 13:00:41 +0900

    chore: cleanup pages

--- src/popup/app.tsx
+++ src/popup/app.tsx
@@ -1,20 +1,11 @@
-import React from "react";
 import browser from "webextension-polyfill";
 
 export function App() {
-  const [counter, setCounter] = React.useState(0);
-
   return (
     <div className="w-[200px] flex flex-col gap-2">
-      <div className="text-lg m-0">Popup</div>
-      <div className="flex items-center">
-        <div>Counter = {counter}</div>
-        <div className="flex-1"></div>
-        <div className="flex gap-1">
-          <button onClick={() => setCounter(counter + 1)}>-1</button>
-          <button onClick={() => setCounter(counter - 1)}>+1</button>
-        </div>
-      </div>
+      <button>Save all tabs</button>
+      <button>Save current tab</button>
+
       <button
         onClick={() => {
           browser.runtime.openOptionsPage();
