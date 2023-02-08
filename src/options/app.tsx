3d1374ee079168b2de675c7282335e9a31c7dd0f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-09 00:20:07 +0900

    wip

--- src/options/app.tsx
+++ src/options/app.tsx
@@ -145,56 +145,31 @@ export class ClientDataImmer {
   //
 
   id: number = 0;
-  groups: SavedTabGroup[] = [
-    {
-      id: "1",
-      createdAt: new Date("2023-02-07T01:23:45"),
-      tabs: [
-        {
-          url: "https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json",
-          title: "manifest.json - Mozilla | MDN",
-        },
-        {
-          url: "https://developer.chrome.com/docs/extensions/mv3/manifest/",
-          title: "Manifest file format - Chrome Developers",
-        },
-      ],
-    },
-    {
-      id: "2",
-      createdAt: new Date("2023-02-05T12:34:56"),
-      tabs: [
-        {
-          url: "https://github.com/hi-ogawa/web-ext-vite-template/pull/5",
-          title:
-            "feat: tab manager by hi-ogawa · Pull Request #5 · hi-ogawa/web-ext-vite-template",
-        },
-      ],
-    },
-  ];
+  groups: SavedTabGroup[] = [...MOCK_DATA];
 
   //
-  // persist/sync via browser.storage.local
+  // load/save via browser.storage.local
   //
 
-  async load() {
+  static async load(): Promise<ClientDataImmer> {
     const record = await browser.storage.local.get(CLIENT_DATA_KEY);
     const serialized = record[CLIENT_DATA_KEY];
+    const instance = new ClientDataImmer();
     if (serialized) {
-      const deserialized = superjson.parse(serialized);
-      console.log("load", { serialized, deserialized });
-      Object.assign(this, superjson.parse(serialized));
+      Object.assign(instance, superjson.parse(serialized));
     }
+    return instance;
   }
 
-  async save() {
+  async save(): Promise<void> {
     const serialized = superjson.stringify(this);
-    console.log("save", { this: this, serialized });
     await browser.storage.local.set({ [CLIENT_DATA_KEY]: serialized });
   }
 
-  subscribe() {
-    const handler = () => this.load();
+  static subscribe(onChange: (value: ClientDataImmer) => void) {
+    const handler = async () => {
+      onChange(await this.load());
+    };
     browser.storage.local.onChanged.addListener(handler);
     return () => {
       browser.storage.local.onChanged.removeListener(handler);
@@ -205,7 +180,7 @@ export class ClientDataImmer {
   // api
   //
 
-  addGroup(tabs: browser.Tabs.Tab[]) {
+  addGroup(tabs: browser.Tabs.Tab[]): ClientDataImmer {
     return produce(this, (self) => {
       const group: SavedTabGroup = {
         id: String(self.id++),
@@ -226,42 +201,6 @@ export class ClientDataImmer {
     id;
   }
 }
-
-browser.runtime.sendMessage;
-browser.runtime.onMessage;
-
-/*
-
-- generalize comlink?
-  - how to listen to service event? (i.e. register callbacks?)
-    - https://github.com/GoogleChromeLabs/comlink#callbacks
-  - it only needs to implement `Endpoint`
-    - e.g. https://github.com/GoogleChromeLabs/comlink/blob/main/src/node-adapter.ts
-
-//
-// service
-//
-const service = {
-  sayHello: (name: string) => "hello " + name,
-}
-anylink.expose(service, serviceLink);
-
-//
-// proxy
-//
-const proxy = anylink.wrap<typeof service>(proxyLink);
-
-//
-// example
-//
-proxy.sayHello("goodbye") -->
-  proxyLink.postMessage({ id: "xxx", method: "sayHello", arguments: ["goodbye"] }) -->
-    serviceLink.onMessage({ ... }) ->
-      service.sayHello("goodbye")
-      serviceLink.postMessage({ id: "xxx", result: "hello goodbye" }) -->
-        proxyLink.onMessage({ ... }) -->
-          return "hello goodbye"
-*/
 
 //
 // global instance
