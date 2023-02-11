f02f2c943dd048d001ee548d2af0ab64d01bd729
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-12 01:56:43 +0900

    chore: differentiate icons for dev (#8)

--- misc/generate-icon.sh
+++ misc/generate-icon.sh
@@ -2,13 +2,18 @@
 
 # generate png icons with different sizes using https://remixicon.com/
 # usage:
-#   bash misc/generate-icon.sh flask-line
+#   bash misc/generate-icon.sh folder-4-fill src/dev/assets
+#   bash misc/generate-icon.sh folder-4-line src/dev/assets/production
 
 icon_name="$1"
+out_dir="$2"
+
 icon_svg=$(jq -r '.icons."'"$icon_name"'".body' < node_modules/@iconify-json/ri/icons.json)
 
-mkdir -p src/dev/assets
+mkdir -p "$out_dir"
+
+echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">'"$icon_svg"'</svg>' > "$out_dir/icon.svg"
+
 for px in 16 32 48 128; do
-  echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">'"$icon_svg"'</svg>' \
-  | convert -density 1000 -resize "${px}x${px}" -background none svg:- "src/dev/assets/icon-${px}.png"
+  convert -density 1000 -resize "${px}x${px}" -background none "$out_dir/icon.svg" "$out_dir/icon-${px}.png"
 done
